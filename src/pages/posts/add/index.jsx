467abb9d434bbc1752f2cfe7bd717169884403d8
Сергей Михалев <mikhalev@MacBook-Pro-Mikhalev.local>
2024-05-13 12:52:43 +0300

    Create page Add new post

--- src/pages/posts/add/index.jsx
+++ src/pages/posts/add/index.jsx
@@ -1,3 +1,4 @@
 import React from "react";
+import { PostForm } from "../components/PostForm";
 
-export const AddPostPage = () => <div>Это страница добавления постов</div>
+export const AddPostPage = () => <PostForm/>
