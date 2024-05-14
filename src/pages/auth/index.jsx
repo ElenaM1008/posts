2c692328532d4280bf01de31026acd6400463163
Сергей Михалев <mikhalev@MacBook-Pro-Mikhalev.local>
2024-05-14 20:17:53 +0300

    Add auth

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,3 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container } from "../../components/ui/Container";
+import { Typo } from "../../components/ui/Typo";
+import { Form } from "../../components/ui/Form";
+import { Field } from "../../components/ui/Field";
+import { Input } from "../../components/ui/Input";
+import { useDispatch } from "react-redux";
+import { login } from '../../redux/slices/authSlices'
 
-export const AuthPage = () => <div>Это страница авторизации</div>
+export const AuthPage = () => {
+	const [formValues, setFormValues] = useState({ name: '', surname: '', email: '', password: '' })
+	const navigate = useNavigate()
+	const dispatch = useDispatch()
+
+	const onChange = (name, value) => {
+		setFormValues({ ...formValues, [name]: value })
+	}
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+		try {
+			const users = JSON.parse(localStorage.getItem('users'))
+			if (!users) {
+				alert("Данный пользователь не найден в системе")
+				return
+			}
+
+			const currentUser = users.find((user) => user.email === formValues.email && user.password === formValues.password)
+
+			if (!currentUser) {
+				alert("Данный пользователь не найден в системе")
+				return
+			}
+
+			dispatch(login(currentUser))
+			navigate('/posts')
+
+
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	const disabled = !formValues.email || !formValues.password
+
+	return (
+		<Container>
+			<Typo>Страница авторизации</Typo>
+			<Form onSubmit={onSubmit}>
+				<Field>
+					<Input
+						type="text"
+						name="email"
+						value={formValues.email}
+						placeholder="E-mail"
+						onChange={(e) => onChange(e.target.name, e.target.value)}
+					/>
+				</Field>
+				<Field>
+					<Input
+						type="text"
+						name="password"
+						value={formValues.password}
+						placeholder="Пароль"
+						onChange={(e) => onChange(e.target.name, e.target.value)}
+					/>
+				</Field>
+				<button type="submit" disabled={disabled}>Регистрация</button>
+			</Form>
+		</Container>
+
+	)
+}
